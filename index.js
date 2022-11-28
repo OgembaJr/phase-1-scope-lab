33c298d6581789bc4c0fb4eddb1813294fc8fb50
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2022-11-28 13:28:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+
+}
+
+function overWriteBestCustomer(){
+   overWriteBestCustomer = 'maybe Bob';
+}
+
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Manuh'
+}
